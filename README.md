7cff11f43b8cef903221630a51f27f0f4666bd16
nyable <loser@88.com>
2024-05-23 10:25:45 +0800

    更新说明

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 一些关于[Directory Opus 12](https://www.gpsoft.com.au/index.html)的设置、脚本、自定义功能记录。
 
 ## 目录划分
-* script: 用于Opus的JScript或VBScript脚本
+* [script](https://github.com/nyable/DirectoryOpus-Config/blob/main/script/README.md): 用于Opus的JScript或VBScript脚本
 * res: 导出dcf文件
 * bin: dcf或其他功能依赖的相关可执行文件或bat脚本
--- script/SmartExtract.js
+++ script/SmartExtract.js
@@ -4,12 +4,16 @@
 // This is a script for Directory Opus.
 // See https://www.gpsoft.com.au/endpoints/redirect.php?page=scripts for development information.
 
-// 增加一个内部命令`SmartExtract`，用于解压时解决同名文件夹的嵌套问题.
-// eg:解压单个文件`SmartExtract 压缩文件A的全路径`=>`SmartExtract C:/Users/nyable/Downloads/A.zip`
-// eg:解压多个文件`SmartExtract 压缩文件A的全路径 压缩文件B的全路径`=>`SmartExtract C:/Users/nyable/Downloads/A.zip C:/Users/nyable/Downloads/B.zip`
-// 如果压缩文件根目录是单个文件夹,且与压缩文件同名,则将文件夹直接解压出来
-// 其他情况均解压到与压缩文件同名的文件夹内
-// 如果已经存在同名文件夹,则会提示是否覆盖,而不会重命名文件夹
+/**
+ 增加一个内部命令`SmartExtract`，用于解决解压时文件没有文件夹或同名文件夹嵌套问题。类似 Banzip 的自动解压。
+
+- eg:解压单个文件`SmartExtract 压缩文件A的全路径`=>`SmartExtract C:/Users/nyable/Downloads/A.zip`
+- eg:解压多个文件`SmartExtract 压缩文件A的全路径 压缩文件B的全路径`=>`SmartExtract C:/Users/nyable/Downloads/A.zip C:/Users/nyable/Downloads/B.zip`
+
+* 如果压缩文件根目录是单个文件夹,且与压缩文件同名,则将文件夹直接解压出来
+* 其他情况均解压到与压缩文件同名的文件夹内
+* 如果已经存在同名文件夹,则会提示是否覆盖,而不会重命名文件夹
+ */
 
 // Called by Directory Opus to initialize the script
 function OnInit (initData) {
